417be5cd2bf0f785d5abc3a2c5118c3a8474c972
kubo39 <kubo39@gmail.com>
2015-04-15 01:55:07 +0900

    Add approxeq.d

---
+++ source/dgeom/approxeq.d
@@ -0,0 +1,29 @@
+module dgeom.approxeq;
+
+import std.math : abs, E;
+
+
+template approx(T) 
+{
+  static  if ( __traits(isFloating, T) ) {
+    T approx_epsilon() @property
+    {
+      return 1.0 * E - 6;
+    }
+
+    bool approx(T other) @property
+    {
+      return approx_eq_eps(other, 1.0 * E - 6);
+    }
+
+    bool approx_eq_eps(T self, T other)
+    {
+      return abs(self - other) < approx_epsilon;
+    }
+  }
+  else static assert(false);
+}
+
+
+mixin approx!float;
+mixin approx!double;
