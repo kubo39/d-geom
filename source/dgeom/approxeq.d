68dcbcbaa005f11e7b7054d48b74c4928df6f526
kubo39 <kubo39@gmail.com>
2015-12-28 18:34:38 +0900

    Up.

--- source/dgeom/approxeq.d
+++ source/dgeom/approxeq.d
@@ -3,25 +3,22 @@ module dgeom.approxeq;
 import std.math : abs, E;
 
 
-template approx(T)
+template approx(T) if ( __traits(isFloating, T) )
 {
-  static  if ( __traits(isFloating, T) ) {
-    T approx_epsilon() @property
-    {
-      return 1.0 * E - 6;
-    }
-
-    bool approx(T other) @property
-    {
-      return approx_eq_eps(other, 1.0 * E - 6);
-    }
-
-    bool approx_eq_eps(T self, T other)
-    {
-      return abs(self - other) < approx_epsilon;
-    }
+  T approx_epsilon() @property
+  {
+    return 1.0 * E - 6;
   }
-  else static assert(false);
+
+  bool approx(T other) @property
+  {
+    return approx_eq_eps(other, 1.0 * E - 6);
+  }
+
+  bool approx_eq_eps(T self, T other)
+  {
+    return abs(self - other) < approx_epsilon;
+  }
 }
 
 
