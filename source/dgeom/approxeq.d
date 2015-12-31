fad7948c5f5059f109b6ce57bc4c01bd36126bce
kubo39 <kubo39@gmail.com>
2015-12-31 21:53:08 +0900

    Camelize.

--- source/dgeom/approxeq.d
+++ source/dgeom/approxeq.d
@@ -5,19 +5,19 @@ import std.math : abs, E;
 
 template approx(T) if ( __traits(isFloating, T) )
 {
-  T approx_epsilon() @property
+  T approxEpsilon() @property
   {
     return 1.0 * E - 6;
   }
 
   bool approx(T other) @property
   {
-    return approx_eq_eps(other, 1.0 * E - 6);
+    return approxEqEps(other, 1.0 * E - 6);
   }
 
-  bool approx_eq_eps(T self, T other)
+  bool approxEqEps(T self, T other)
   {
-    return abs(self - other) < approx_epsilon;
+    return abs(self - other) < approxEpsilon;
   }
 }
 
