da790c4774ca40e98179416f53ca52d88b886524
kubo39 <kubo39@gmail.com>
2016-03-06 04:43:05 +0900

    Add some attributes.

--- source/dgeom/approxeq.d
+++ source/dgeom/approxeq.d
@@ -5,17 +5,17 @@ import std.math : abs, E;
 
 template approx(T) if ( __traits(isFloating, T) )
 {
-  T approxEpsilon() @property
+  T approxEpsilon() @property @nogc @safe nothrow pure
   {
     return 1.0 * E - 6;
   }
 
-  bool approx(T other) @property
+  bool approx(T other) @property @nogc @safe pure
   {
     return approxEqEps(other, 1.0 * E - 6);
   }
 
-  bool approxEqEps(T self, T other)
+  bool approxEqEps(T self, T other) @nogc @safe pure
   {
     return abs(self - other) < approxEpsilon;
   }
